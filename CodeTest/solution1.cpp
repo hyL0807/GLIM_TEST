64ed8d2b655e61eac4d52655573e656192eab9e5
hy_0807 <awe080798@gmail.com>
2024-12-27 23:40:42 +0900

    upload coding test

---
+++ CodeTest/solution1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int k) {
+    int answer = 0;
+
+    string ab = to_string(a) + to_string(b);
+    int ab = stoi(ab);
+
+    return answer;
+}
+int main(void){
+    printf(solution(10,3));
+
+}
